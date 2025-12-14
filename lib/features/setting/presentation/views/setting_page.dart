6233077529f0fae5fa67fda87e98ee9d72405918
fauzihub13 <fauziadisaputra3@gmail.com>
2025-12-15 04:01:33 +0700

    feat: create reusable modal logout

--- lib/features/setting/presentation/views/setting_page.dart
+++ lib/features/setting/presentation/views/setting_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:litenet/core/constants/theme.dart';
-import 'package:litenet/core/widgets/button.dart';
 import 'package:litenet/core/widgets/custom_appbar.dart';
+import 'package:litenet/core/widgets/custom_modal.dart';
 import 'package:litenet/features/setting/presentation/views/change_password.dart';
 import 'package:litenet/features/setting/presentation/views/change_profile.dart';
 import 'package:litenet/features/setting/presentation/views/faq_page.dart';
@@ -17,82 +17,6 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
-  void _showLogoutModal(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      backgroundColor: Colors.transparent, // Agar rounded corner terlihat
-      isScrollControlled: true,
-      builder: (context) {
-        return Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-          ),
-          padding: const EdgeInsets.only(
-            left: 24,
-            right: 24,
-            bottom: 12,
-            top: 12,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: 60,
-                height: 4,
-                decoration: BoxDecoration(
-                  color: DefaultColors.purple200,
-                  borderRadius: BorderRadius.all(Radius.circular(2)),
-                ),
-              ),
-              const SizedBox(height: 20),
-
-              Text(
-                "Yakin keluar dari akun Anda?",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                  fontWeight: FontWeight.w600,
-                  color: DefaultColors.purple500,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  // Tombol Tidak
-                  Expanded(
-                    child: Button(
-                      text: "Tidak",
-                      buttonType: ButtonType.filled,
-                      backgroundColor: DefaultColors.purple50,
-                      textColor: DefaultColors.purple500,
-                      onPressed: () => Navigator.pop(context),
-                    ),
-                  ),
-
-                  const SizedBox(width: 16),
-
-                  // Tombol Ya, Keluar
-                  Expanded(
-                    child: Button(
-                      text: "Ya, keluar",
-                      buttonType: ButtonType.filled,
-                      onPressed: () {
-                        // Tambahkan logika logout di sini (clear session, dll)
-                        // Contoh: pindah ke halaman login
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20), // Memberikan padding bawah agar aman
-            ],
-          ),
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -200,7 +124,7 @@ class _SettingPageState extends State<SettingPage> {
                         DefaultColors.lightRed, // Warna merah untuk keluar
                     iconColor: DefaultColors.lightRed,
                     onTap: () {
-                      _showLogoutModal(context);
+                      showLogoutModal(context);
                     },
                   ),
                 ],
