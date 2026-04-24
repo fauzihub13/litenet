bed2bae3b59769a9dd0dda83631c582414e3923f
fauzihub13 <fauziadisaputra3@gmail.com>
2026-04-24 21:25:40 +0700

    feat time

--- lib/core/extensions/datetime_context.ext.dart
+++ lib/core/extensions/datetime_context.ext.dart
@@ -1,7 +1,6 @@
 import 'package:intl/intl.dart';
 
 extension DateTimeFormatID on DateTime {
-
   String toIndonesianDateString() {
     // locale 'id' : bahasa Indonesia
     return DateFormat("d MMMM yyyy", "id").format(this);
@@ -14,7 +13,7 @@ extension DateTimeFormatID on DateTime {
 
   String toIndonesianDateTimeStringPlus7() {
     // locale 'id' : bahasa Indonesia
-    final jakartaTime = this.add(const Duration(hours: 7));
+    final jakartaTime = add(const Duration(hours: 7));
     return DateFormat("d MMMM yyyy HH:mm", "id").format(jakartaTime);
   }
 }
